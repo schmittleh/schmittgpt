0908fe6a82ece65b13fdde8992e3db64c439a1b5
leticia schmitt <99880251+schmittleh@users.noreply.github.com>
2023-07-26 16:15:57 -0300

    nova conversa

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import IconClose from "./icons/iconClose";
+import IconAdd from "./icons/iconAdd";
 
 type Props = {
     children: ReactNode;
@@ -16,10 +17,17 @@ export const Sidebar = ({open, onClose, children }: Props)  => {
         ${open ? 'w-screen bg-gray-600/75' : 'w-0'} md:w-64`}>
 
             <div className={` transition-all duration-200  flex  h-screen 
-            ${open ? 'ml-0' : '-ml-96'}`}>
+            ${open ? 'ml-0' : '-ml-96'} md:ml-0`}>
 
-              <div className="">
-                barra mesmo
+              <div className="flex flex-col w-64  p-2 bg-gray-900">
+                
+                    <div className="">
+                 <IconAdd width={16} height={16} />
+                        Nova conversa
+                    </div>
+
+
+
               </div>
                     <div onClick= {onClose} className=" flex justify-center items-center w-10 h-10 cursor-pointer md:hidden">
                     <IconClose width={24} height={24} />
